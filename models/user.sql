b332c6c0df8b2fdf8fadd2b60b84a2a37256b69c
Sree Teja <sreeteja.muthyala@gmail.com>
2019-03-25 20:39:32 +0530

    sql intial commit

---
+++ models/user.sql
@@ -0,0 +1,41 @@
+-- drop database rhdpTest;
+-- GO
+
+-- create database rhdpTest;
+-- GO
+
+use rhdpTest;
+GO
+
+create table users(
+  userID int not null primary key,
+  email varchar(60) not null,
+  password varchar(300) not null,
+  name varchar(50) not null,
+  phone int not null,
+);
+GO
+
+create table tokens(
+  userID int not null,
+  token varchar(300) not null,
+  foreign key (userID) references users(userID),
+  access varchar(100) not null,
+);
+GO
+
+create procedure removeToken
+  @inToken varchar(300)
+  AS
+  BEGIN
+    delete from tokens where token = @inToken;
+  END
+GO
+
+create procedure findByToken
+@inToken varchar(300)
+AS
+  BEGIN
+    select userID from tokens where token = @inToken;
+  END
+GO
--- test/temp.js
+++
@@ -1,3 +0,0 @@
-const { center_geolocation} = require("../helpers/center_geolocation");
-const  locations = require("./locations.json");
-console.log(center_geolocation(locations));
