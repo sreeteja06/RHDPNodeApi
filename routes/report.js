687cc7786089ec09017179609cfb3a46ff20376a
Sree Teja <sreeteja.muthyala@gmail.com>
2019-11-03 11:52:33 +0530

    removed html-pdf

--- routes/report.js
+++ routes/report.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const pdf = require("html-pdf");
+// const pdf = require("html-pdf");
 var fs = require("fs");
 var {PythonShell} = require("python-shell");
 
