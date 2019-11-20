99ae33de2d47c1cb55fb0655a114b03511b8025c
sreeteja06 <sreeteja.muthyala@gmail.com>
2019-11-20 16:56:48 +0530

    node cache

--- routes/statistics.js
+++ routes/statistics.js
@@ -5,10 +5,14 @@ const express = require('express');
 
 const router = express.Router();
 
+const NodeCache = require('node-cache');
+
 const { findDensity, findAvgTime } = require('../helpers/statisticsHelper');
 const { authenticate } = require('../middleware/authenticate');
 const { poolPromise } = require('../db/sql_connect');
 const { poolPromise2 } = require('../db/sql_connect2');
+
+const myCache = new NodeCache();
 
 const getTimeInMinutes = timeF => {
   // past 15 mins
@@ -46,67 +50,74 @@ router.post(
   '/getDensity',
   authenticate,
   awaitHandler(async (req, res) => {
-    let pool;
-    try {
-      console.log('Get density endpoint');
-      pool = await poolPromise;
-      const jidds = await pool
-        .request()
-        .query(
-          `select junctionPoint.JID, junctionName from junctionPoint, jAccess where junctionPoint.JID = jAccess.JID and jAccess.UserId = ${req.userID}`
-        );
-      const jids = jidds.recordset.map(x => x.JID);
-      const { timeF } = req.body;
-      const timeCondition = getTimeInMinutes(timeF);
-      let result = [];
-      if (timeF == 3 || timeF == 4 || timeF == 5) {
-        result = await pool.request().query(
-          // set @NOWDATE = DATEADD(HH, +5, GETDATE());
-          `DECLARE @NOWDATE DATETIME;
+    const mCacheKey = `getDensity${req.body.timeF}`;
+    const mCacheValue = myCache.get(mCacheKey);
+    if (mCacheValue) {
+      res.send(JSON.parse(mCacheValue));
+    } else {
+      let pool;
+      try {
+        console.log('Get density endpoint');
+        pool = await poolPromise;
+        const jidds = await pool
+          .request()
+          .query(
+            `select junctionPoint.JID, junctionName from junctionPoint, jAccess where junctionPoint.JID = jAccess.JID and jAccess.UserId = ${req.userID}`
+          );
+        const jids = jidds.recordset.map(x => x.JID);
+        const { timeF } = req.body;
+        const timeCondition = getTimeInMinutes(timeF);
+        let result = [];
+        if (timeF == 3 || timeF == 4 || timeF == 5) {
+          result = await pool.request().query(
+            // set @NOWDATE = DATEADD(HH, +5, GETDATE());
+            `DECLARE @NOWDATE DATETIME;
         set @NOWDATE = DATEADD(HH, +5, CONVERT(datetime, '06-09-2019  6:00:00 PM'));
         set @NOWDATE = DATEADD(n, +30, @NOWDATE);
         select * from LogDelhi where UID in (${jids.toString()}) 
         and Upload_Time > ${timeCondition}`
-        );
-      } else {
-        // set @NOWDATE = DATEADD(HH, +5, GETDATE());
-        result = await pool.request().query(
-          `DECLARE @NOWDATE DATETIME;
+          );
+        } else {
+          // set @NOWDATE = DATEADD(HH, +5, GETDATE());
+          result = await pool.request().query(
+            `DECLARE @NOWDATE DATETIME;
         set @NOWDATE = DATEADD(HH, +5, CONVERT(datetime, '06-09-2019  6:00:00 PM'));
         set @NOWDATE = DATEADD(n, +30, @NOWDATE);
         set @NOWDATE = DATEADD(mi, -${timeCondition}, @NOWDATE);
         select * from LogDelhi where UID in (${jids.toString()}) 
         and Upload_Time > @NOWDATE`
-        );
-      }
-      const parsedObj = {};
-      jids.forEach(e => {
-        parsedObj[e] = [];
-      });
-      const resObj = {};
-      result.recordset.forEach(e => {
-        parsedObj[e.UID].push(e.Message);
-      });
-      for (const key in parsedObj) {
-        if (parsedObj[key] !== undefined && parsedObj[key].length > 0) {
-          resObj[key] = findDensity(parsedObj[key]);
+          );
         }
+        const parsedObj = {};
+        jids.forEach(e => {
+          parsedObj[e] = [];
+        });
+        const resObj = {};
+        result.recordset.forEach(e => {
+          parsedObj[e.UID].push(e.Message);
+        });
+        for (const key in parsedObj) {
+          if (parsedObj[key] !== undefined && parsedObj[key].length > 0) {
+            resObj[key] = findDensity(parsedObj[key]);
+          }
+        }
+        const formatRes = [];
+        for (const key in resObj) {
+          const temp = {};
+          // temp["JID"] = key;
+          temp.intersectionName = jidds.recordset.find(e => {
+            return e.JID == key;
+          }).junctionName;
+          temp.avgDensity = resObj[key];
+          formatRes.push(temp);
+        }
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        myCache.set(mCacheKey, JSON.stringify(formatRes), 300);
+        res.send(formatRes);
+      } catch (e) {
+        console.log(e);
+        res.sendStatus(500).end();
       }
-      const formatRes = [];
-      for (const key in resObj) {
-        const temp = {};
-        // temp["JID"] = key;
-        temp.intersectionName = jidds.recordset.find(e => {
-          return e.JID == key;
-        }).junctionName;
-        temp.avgDensity = resObj[key];
-        formatRes.push(temp);
-      }
-      res.setHeader('Content-Type', 'application/json; charset=utf-8');
-      res.send(formatRes);
-    } catch (e) {
-      console.log(e);
-      res.sendStatus(500).end();
     }
   })
 );
@@ -115,70 +126,77 @@ router.post(
   '/getAvgTime',
   authenticate,
   awaitHandler(async (req, res) => {
-    let pool;
-    try {
-      pool = await poolPromise;
-      let jidds = await pool
-        .request()
-        .query(
-          `select junctionPoint.JID from junctionPoint, jAccess where junctionPoint.JID = jAccess.JID and jAccess.UserId = ${req.userID}`
-        );
-      jidds = jidds.recordset.map(x => x.JID);
-      const jids = req.body.interID;
-      if (
-        jidds.find(e => {
-          return e == jids;
-        }) === undefined
-      ) {
-        throw '203';
-      }
-      // timeF - timeframe information is in getTimeInMinutes function
-      const { timeF } = req.body;
-      const timeCondition = getTimeInMinutes(timeF);
-      let result = [];
-      if (timeF == 3 || timeF == 4 || timeF == 5) {
-        // set @NOWDATE = DATEADD(HH, +5, GETDATE());
-        result = await pool.request().query(
-          `DECLARE @NOWDATE DATETIME;
+    const mCacheKey = `getDensity${req.body.timeF}${req.body.interID}${req.body.grpBy}`;
+    const mCacheValue = myCache.get(mCacheKey);
+    if (mCacheValue) {
+      res.send(JSON.parse(mCacheValue));
+    } else {
+      let pool;
+      try {
+        pool = await poolPromise;
+        let jidds = await pool
+          .request()
+          .query(
+            `select junctionPoint.JID from junctionPoint, jAccess where junctionPoint.JID = jAccess.JID and jAccess.UserId = ${req.userID}`
+          );
+        jidds = jidds.recordset.map(x => x.JID);
+        const jids = req.body.interID;
+        if (
+          jidds.find(e => {
+            return e == jids;
+          }) === undefined
+        ) {
+          throw '203';
+        }
+        // timeF - timeframe information is in getTimeInMinutes function
+        const { timeF } = req.body;
+        const timeCondition = getTimeInMinutes(timeF);
+        let result = [];
+        if (timeF == 3 || timeF == 4 || timeF == 5) {
+          // set @NOWDATE = DATEADD(HH, +5, GETDATE());
+          result = await pool.request().query(
+            `DECLARE @NOWDATE DATETIME;
         set @NOWDATE = DATEADD(HH, +5, CONVERT(datetime, '06-09-2019  6:00:00 PM'));
         set @NOWDATE = DATEADD(n, +30, @NOWDATE);
         select * from LogDelhi where UID in (${jids.toString()}) 
         and Upload_Time > ${timeCondition}`
-        );
-      } else {
-        // set @NOWDATE = DATEADD(HH, +5, GETDATE());
-        result = await pool.request().query(
-          `DECLARE @NOWDATE DATETIME;
+          );
+        } else {
+          // set @NOWDATE = DATEADD(HH, +5, GETDATE());
+          result = await pool.request().query(
+            `DECLARE @NOWDATE DATETIME;
         set @NOWDATE = DATEADD(HH, +5, CONVERT(datetime, '06-09-2019  6:00:00 PM'));
         set @NOWDATE = DATEADD(n, +30, @NOWDATE);
         set @NOWDATE = DATEADD(mi, -${timeCondition}, @NOWDATE);
         select * from LogDelhi where UID in (${jids.toString()}) 
         and Upload_Time > @NOWDATE`
-        );
-      }
-      result = result.recordset.map(e => {
-        return { Upload_Time: e.Upload_Time, Message: e.Message };
-      });
-      // grpBy use in statisticdHelper page
-      let Sendres = findAvgTime(result, req.body.grpBy);
-      Sendres = Sendres.map(e => {
-        const temp = e.find(x => {
-          return x != null;
-        });
-        if (temp) {
-          return e;
-        } else {
-          return [];
+          );
         }
-      });
-      res.setHeader('Content-Type', 'application/json; charset=utf-8');
-      res.send(Sendres);
-    } catch (e) {
-      console.log(e);
-      if (e == 203) {
-        res.sendStatus(203).send({ err: 'unauthorized Junction ID' });
-      } else {
-        res.sendStatus(500).end();
+        result = result.recordset.map(e => {
+          return { Upload_Time: e.Upload_Time, Message: e.Message };
+        });
+        // grpBy use in statisticdHelper page
+        let Sendres = findAvgTime(result, req.body.grpBy);
+        Sendres = Sendres.map(e => {
+          const temp = e.find(x => {
+            return x != null;
+          });
+          if (temp) {
+            return e;
+          } else {
+            return [];
+          }
+        });
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        myCache.set(mCacheKey, JSON.stringify(Sendres), 300);
+        res.send(Sendres);
+      } catch (e) {
+        console.log(e);
+        if (e == 203) {
+          res.sendStatus(203).send({ err: 'unauthorized Junction ID' });
+        } else {
+          res.sendStatus(500).end();
+        }
       }
     }
   })
